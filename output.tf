53f9b8d4bc5a536d61c5bbd59be239a6f905dfb2
Dejun Xiang <derek@Dejuns-MBP.gateway>
2022-10-14 09:27:47 +1100

    Remove unused code block

--- output.tf
+++ output.tf
@@ -2,6 +2,6 @@ output "google_project" {
   value = data.google_project.project
 }
 
-output "artifact-registry-location" {
-  value = "${google_artifact_registry_repository.repository.location}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.repository.repository_id}/"
-}
+# output "artifact-registry-location" {
+#   value = "${google_artifact_registry_repository.repository.location}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.repository.repository_id}/"
+# }
